2de2eeb961c35d6c3017674cf5eb693f1c1bf9a8
runeberg <runeberg@outlook.de>
2017-10-31 21:21:19 +0100

    Simple C++ Gaze Component implementation

---
+++ Source/LivestreamVR/GazeComponent.cpp
@@ -0,0 +1,56 @@
+// MIT License [https://opensource.org/licenses/MIT] Copyright (c) 2017 runeberg.io
+
+#include "GazeComponent.h"
+
+
+// Sets default values for this component's properties
+UGazeComponent::UGazeComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called every frame
+void UGazeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Implement gaze function
+
+	// Check if we're gazing
+	if (bIsGazing)
+	{
+		// Check if we need to draw a debug line
+		if (bDrawDebug)
+		{
+			// Draw a debug line 
+			UKismetSystemLibrary::DrawDebugLine(this,
+				this->GetComponentLocation() + HMDOffset,
+				this->GetComponentLocation() + (this->GetComponentRotation().Vector() * GazeDistance),
+				FColor(255.f, 0.f, 0.f),
+				0.f,
+				15.f);
+		}
+
+		// Do a line trace
+		FHitResult OutHit;
+		bool bHasHit = GetWorld()->LineTraceSingleByChannel(OutHit, this->GetComponentLocation(),
+			this->GetComponentLocation() + (this->GetComponentRotation().Vector() * GazeDistance),
+			ECollisionChannel::ECC_Visibility
+			);
+
+
+		// Check if we have a hit
+		if (bHasHit && OutHit.GetActor()->IsValidLowLevel())
+		{
+			GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1.f, FColor(0, 255, 0), TEXT("A hit from C++"), false);
+			UE_LOG(LogTemp, Warning, TEXT("TEST"));
+		}
+	}
+		
+}
+
---
+++ Source/LivestreamVR/GazeComponent.h
@@ -0,0 +1,37 @@
+// MIT License [https://opensource.org/licenses/MIT] Copyright (c) 2017 runeberg.io
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Engine/Engine.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "GazeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class LIVESTREAMVR_API UGazeComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGazeComponent();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VR")
+	bool bIsGazing = true;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VR")
+	bool bDrawDebug = true;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VR")
+	float GazeDistance = 500.f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "VR")
+	FVector HMDOffset = FVector(0.f, 0.f, 25.f);
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+};
